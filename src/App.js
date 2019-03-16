cc1d2c8230ed3b69ac29d19a459f664e73d6eac3
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-03-16 18:00:48 +0100

    Install React Router. Conversion classic semantic on new components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React, { Component } from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div>React Router</div>;
+    return (
+      <BrowserRouter>
+        <>
+          <header>
+            <nav>
+              <ul>
+                {/* <li>
+                  <a href="http:/start">Home</a>
+                </li>
+                <li>
+                  <a href="http:/news">News</a>
+                </li>
+                <li>
+                  <a href="http:/contact">Contact</a>
+                </li> */}
+                <li>
+                  <Link to="http:/start">Home</Link>
+                </li>
+                <li>
+                  <Link to="http:/news">News</Link>
+                </li>
+                <li>
+                  <Link to="http:/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <section>Hello World!</section>
+        </>
+      </BrowserRouter>
+    );
   }
 }
 
